c9fc5c7f05f60154c9bf142ad01d640085c1134d
schavis2022 <schavis2022>
2022-12-30 00:03:03 -0500

    Test Driven Dev. C32 Ex1

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,77 +1,102 @@
+using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
 using System;
 using Xunit;
+using Xunit.Sdk;
 
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 2, 3, 6)]
+        [InlineData(5, 5, 5, 15)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            var actual = test.Add(num1, num2, num3);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5,5,0)]
+        [InlineData(-5, 20, 15)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var tester = new UnitTestMethods();
+
 
             //Act
+            var actual = tester.Subtract(minuend, subtrhend);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2,2,4)]
+        [InlineData(5, 4, 20)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            var actual = test.Multiply(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
+
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(20, 5, 4)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var tester = new UnitTestMethods();
 
             //Act
+            var actual = tester.Divide(num1, num2);
+
 
             //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+#pragma warning disable xUnit1001 // Fact methods cannot have parameters
+        public void YourMethodName(object tester)
+#pragma warning restore xUnit1001 // Fact methods cannot have parameters
+        {
+            //Arrange
+            var expected = 'h';
+
+            //Act
+            var actual = tester.YourMethodName();
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Fact]
-        public void YourMethodName()
+        public void YourMethodName2(System.Collections.Generic.IEnumerable<object> actual)
         {
             //Arrange
+            var expected = 'h';
 
             //Act
 
-            //Assert
-
-        }
-
-        [Fact]
-        public void YourMethodName2()
-        {
-            //Arrange
-
-            //Act
 
             //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -1,25 +1,52 @@
 ﻿using System;
+using System.Data.Common;
+
 namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
-        // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // Make sure your references are set properly------DONE
+            // Righ-click on Dependencies in your .Tests project and set your reference------DONE
 
 
 
-        // Create an Add method that passes 3 integers
+        // Create an Add method that passes 3 integers-----DONE
+        public int Add(int one, int two, int three)
+        {
+            return one + two + three;
+        }
 
-        // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Create a Subtract method that passes 2 integers-------DONE// NOTE:::Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrhend)
+        {
+            return minuend - subtrhend;
+        }
+            
 
         // Create a Multiply method that passes 2 integers
+        public int Multiply(int one, int two)
+        {
+            return one * two;
+        }
 
         // Create a Divide method that passes 2 integers
+        public int Divide(int one, int two)
+        {
+            return one / two;
+        }
 
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public int YourMethodName()
+        {
+            return 'h';
+        }
+        public int YourMethodName2()
+        {
+            return 'h';
+        }
+
 
         
     }
